596b3959937d6bbc5cd4c222555df912cf0d2961
gvenezia <g.venezia@outlook.com>
2018-02-05 02:47:09 +0000

    adding tweeting functionality, currently receiving a POST error

--- index.js
+++ index.js
@@ -1,13 +1,22 @@
 // IIFE for containing scope of variables
 ;(function(){
+  
+var quoteString = "";
+var quoteAuthor = "";
 
 // Get a new quote on page load 
-$(document).ready(getQuote());
+$(document).ready(function(){
+  getQuote();
+});
   
 //   Get a new quote on button click
-  $(".quote-button").on("click", function(e){
+$("#generate").on("click", function(e){
       getQuote();
-  });
+});
+
+$("#generate").on("click", function(e){
+  tweetQuote();
+});
   
   
   function getQuote(){
@@ -19,19 +28,32 @@ $(document).ready(getQuote());
           var response = JSON.parse(this.responseText);
           
         // Extract the text from the preformatted content (the API put the content in an <p> elements)
-          var quoteString = $(response[0].content).text();
+          quoteString = $(response[0].content).text();
+          quoteAuthor = response[0].title;
           
-        // Put the quote in the 
+          // Put the quote on the page
           $('#quote').html(
-              "<p id='quote-text'>" + quoteString + 
-              " — " + "<span id='quote-author'>" + 
-              response[0].title + "</span></p>"
+              '<span class="comma">"</span><p id="quote-text">' + quoteString + 
+              '<br><span class="comma" id="comma2">"</span><br> ' + "<span id='quote-author'> — " + 
+              quoteAuthor + " — </span></p>"
               ); 
         }
       };
       
       xhttp.open("GET", "https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&callback=" + Math.random(), true);
       xhttp.send();
+  };
+  
+  function tweetQuote(){
+    var xhttp = new XMLHttpRequest();
+      
+    xhttp.onreadystatechange = function () {
+        if (xhttp.readyState == 4 && xhttp.status == 200) {
+          console.log("all good w/ tweetQuote()");
+        }
+    };
+   xhttp.open("POST", "https://api.twitter.com/1.1/statuses/update.json?status=" + quoteString + " –" + quoteAuthor, true);
+   xhttp.send();
   };
   
 }());
